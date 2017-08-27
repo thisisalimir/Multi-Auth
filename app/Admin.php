23f158f3d3b306dbbb4a9728a253fa8ce2eb867e
Ali Fatahi <a.fatahi1992@gmail.com>
2017-08-27 20:14:05 +0430

    add Comment

--- app/Admin.php
+++ app/Admin.php
@@ -5,15 +5,29 @@ namespace App;
 //Reason we use is our custom guard need to be instance Authentication
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class Admin
+ * @package App
+ */
 class Admin extends Authenticatable
 {
+    /**
+     * @var string
+     */
     protected $table = 'admins';
+
+    /**
+     * @var array
+     */
     protected $fillable = [
-        'name' , 'email','password'
+        'name', 'email', 'password'
     ];
 
 
+    /**
+     * @var array
+     */
     protected $hidden = [
-      'password','remember_token'
+        'password', 'remember_token'
     ];
 }
